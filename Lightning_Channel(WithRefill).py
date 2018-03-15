561f36727a2698a06b3cd61e65f6e300d23e5716
Danyal Ahmad <danyal20098@gmail.com>
2018-03-15 19:12:04 +0500

    Refill Mechanism has been introduced in the programs.

---
+++ Lightning_Channel(WithRefill).py
@@ -0,0 +1,151 @@
+import random
+from configparser import ConfigParser
+
+
+# USER CLASS
+class User:
+    def __init__(self, coin, name):
+        self.coins = coin
+        self.name = name
+        self.hash_counter = 0
+        self.signature_counter = 0
+        self.messages = 0
+        self.refil = 0
+
+    def sendCoin(self, amount):
+        self.coins -= amount
+
+    def recieveCoin(self, amount):
+        self.coins += amount
+
+    # Check overflow occurrence before receiving payment
+    def checkOverflow(self, amount):
+        if self.coins + amount < self.coins:
+            return True
+        else:
+            return False
+
+
+def refill(sender,receiver):
+    if 0 == sender.coins:
+        payUser(receiver,sender,10)
+        sender.refil += 1
+        return True;
+    else:
+        return False;
+
+
+def payUser(sender, receiver, amount):
+    if not (receiver.checkOverflow(amount)):
+        print(sender.name, ' computer a hash & a Signature')
+        sender.hash_counter += 1
+        sender.signature_counter += 1
+        sender.messages += 2
+        receiver.hash_counter += 1
+        receiver.signature_counter += 1
+        receiver.messages += 2
+        sender.sendCoin(amount)
+        receiver.recieveCoin(amount)
+        print(sender.name, ' transferred a coin to ', receiver.name)
+    else:
+        print('Overflow occurred')
+
+
+def printResults():
+    print("-------------------LIGHTNING CHANNEL(FILE READ) SIMULATION ENDS--------------------")
+    print(no_payments, ' payments completed.')
+    print('After simulation Alice has ', alice.coins, ' coins and bob has ', bob.coins, ' coins')
+    print("Total Messages transferred were ", alice.messages + bob.messages)
+
+    print("---------------Alices' Statistics---------------")
+    print("Messages transferred by Alice are: ", alice.messages)
+    print("Hashes created by Alice are: ", alice.hash_counter)
+    print("Signatures created by Alice are: ", alice.signature_counter)
+    print("Alice refilled her channel ", alice.refil , " times.")
+
+    print("---------------Bob's Statistics---------------")
+    print("Messages transferred by Bob are: ", bob.messages)
+    print("Hashes created by Bob are: ", bob.hash_counter)
+    print("Signatures created by Bob are: ", bob.signature_counter)
+    print("Bob refilled his channel ", bob.refil , " times.")
+
+
+def coin_Toss():
+    with open('coin_toss.txt', 'r') as f:
+        for line in f:
+            for ch in line:
+                coinTossList.append(int(ch))
+
+
+def writeforGraphs(TotalMessages):
+    with open('LightningResults.txt', 'a') as f:
+        f.write(str(TotalMessages) + "\n")
+
+
+cfg = ConfigParser()
+cfg.read('config.ini')
+
+# variables needed in the simulation
+user1Coins = int(cfg.get('Lightning_Variables', 'user1coins'))
+user2Coins = int(cfg.get('Lightning_Variables', 'user2coins'))
+amount_to_transfer = int(cfg.get('Lightning_Variables', 'amount_to_transfer'))
+_rounds = 0
+unbalancedCondition = 0
+coinTossList = []
+i = 0
+
+# create two objects of the user class
+alice = User(user1Coins, 'alice',)
+bob = User(user2Coins, 'bob',)
+
+# input required for the number of payments
+print("LIGHTNING CHANNEL SIMULATION")
+readFromFile = int(input("Do you want to read coin toss from the file (1 YES , 0 NO): "))
+if readFromFile == 1:
+    coin_Toss()
+    no_payments = len(coinTossList)
+else:
+    no_payments = int(input("Enter the number of payments you want to Simulate: "))
+
+# for loop to run X times
+for x in range(no_payments):
+    _rounds += 1
+    print('ROUND # ', _rounds)
+    # if Alice or Bob gets to an unbalanced state of ZERO coins
+    # Alice has Zero Coins
+
+    if refill(alice,bob):
+        print('Alice Refilled Her Channel')
+
+    elif refill(bob,alice):
+        print('Bob Refilled Her Channel')
+
+    else:
+        if readFromFile == 1:
+            # coin Toss between head and tails.
+            # Head = 1 & tail = 0
+            coinToss = coinTossList[i]
+            # coin toss = 1 = heads . Alice pays bob
+            if 1 == coinToss:
+                payUser(alice, bob, amount_to_transfer)
+            # coin toss = 0 = Tails . Bob pays Alice
+            else:
+                payUser(bob, alice, amount_to_transfer)
+            i += 1
+        else:
+            # coin Toss between head and tails.
+            # Head = 1 & tail = 0
+            coinToss = random.randint(0, 1)
+            # coin toss = 1 = heads . Alice pays bob
+            if 1 == coinToss:
+                payUser(alice, bob, amount_to_transfer)
+            # coin toss = 0 = Tails . Bob pays Alice
+            else:
+                payUser(bob, alice, amount_to_transfer)
+
+    print('New state of coins is that ', alice.name, ' has ', alice.coins, ' coins and ', bob.name, ' has ',
+          bob.coins,
+          ' coins')
+    writeforGraphs(alice.messages + bob.messages)
+
+printResults()
---
+++ Lightning_Duplex(CoinToss)(WithRefill).py
@@ -0,0 +1,208 @@
+import random
+from configparser import ConfigParser
+cfg = ConfigParser()
+cfg.read('config.ini')
+
+# USER CLASS
+class User:
+    def __init__(self, coin, name):
+        self.coins = coin
+        self.deposited_coins = 0
+        self.name = name
+        self.hash_counter = 0
+        self.signature_counter = 0
+        self.messages = 0
+
+    def sendCoin(self, amount):
+        self.coins -= amount
+
+    def recieveCoin(self, amount):
+        self.deposited_coins += amount
+
+    # Check overflow occurrence before receiving payment
+    def checkOverflow(self, amount):
+        if self.coins + amount < self.coins:
+            return True
+        else:
+            return False
+
+
+def onewayChannel(sender, receiver, amount):
+    if sender.coins == 0:
+        return False
+    else:
+        if not (receiver.checkOverflow(amount)):
+            print(sender.name, ' computes 2 Signatures')
+            sender.signature_counter += 2
+            sender.messages += 1
+            sender.sendCoin(amount)
+            receiver.recieveCoin(amount)
+            print(sender.name, ' transferred a coin to ', receiver.name)
+        else:
+            print('Overflow occurred')
+        return True
+
+
+def lightningResetWithPayment(sender, receiver, amount):
+    global lightningResetOccurred
+    if not (receiver.checkOverflow(amount)):
+        print(sender.name, ' computes a hash & a Signature')
+        sender.hash_counter += 1
+        sender.signature_counter += 1
+        sender.messages += 2
+        receiver.signature_counter += 1
+        receiver.hash_counter += 1
+        receiver.messages += 2
+        sender.coins = sender.deposited_coins + sender.coins
+        receiver.coins = receiver.deposited_coins + receiver.coins
+        receiver.deposited_coins = 0
+        sender.deposited_coins = 0
+        print("Lightning reset Occurred now ")
+        lightningResetOccurred += 1
+    if onewayChannel(sender, receiver, amount):
+        pass
+    else:
+        print('Overflow occurred')
+
+
+def lightningResetWithoutPayment(sender, receiver, amount):
+    global lightningResetOccurred
+    if not (receiver.checkOverflow(amount)):
+        print(sender.name, ' computes a hash & a Signature')
+        sender.hash_counter += 1
+        sender.signature_counter += 1
+        sender.messages += 2
+        receiver.signature_counter += 1
+        receiver.hash_counter += 1
+        receiver.messages += 2
+        sender.coins = sender.deposited_coins + sender.coins
+        receiver.coins = receiver.deposited_coins + receiver.coins
+        receiver.deposited_coins = 0
+        sender.deposited_coins = 0
+        print("Lightning reset Occurred now ")
+        lightningResetOccurred += 1
+    else:
+        print('Overflow occurred')
+
+
+def validate_Uncommitted_Coins(sender):
+    if sender.deposited_coins > 0:
+        return True
+    else:
+        return False
+
+
+def refillChannel(sender,receiver):
+    if onewayChannel(sender,receiver,10):
+        return True
+    else:
+        return False
+
+
+def printResults():
+    print("-------------------LIGHTNING DUPLEX WITH COIN TOSS (FILE READ) SIMULATION ENDS--------------------")
+    print(no_payments, ' payments completed.')
+    print("After Simulation:")
+    print('Alice has ', alice.coins, ' coins and bob has ', bob.coins, ' coins')
+    print("Alice has received ", alice.deposited_coins, " uncommitted coin and bob has received ", bob.deposited_coins,
+          " uncommitted coin")
+    print("Refill Occurred in rounds")
+    print(refill_Occurred)
+
+    print("Total Messages transferred were ", alice.messages + bob.messages)
+    print("Lightning Reset Occurred ", lightningResetOccurred, " times.")
+
+    print("---------------Alices' Statistics---------------")
+    print("Messages transferred by Alice are: ", alice.messages)
+    print("Hashes created by Alice are: ", alice.hash_counter)
+    print("Signatures created by Alice are: ", alice.signature_counter)
+
+    print("---------------Bob's Statistics---------------")
+    print("Messages transferred by Bob are: ", bob.messages)
+    print("Hashes created by Bob are: ", bob.hash_counter)
+    print("Signatures created by Bob are: ", bob.signature_counter)
+
+
+def coin_Toss():
+    with open('coin_toss.txt', 'r') as f:
+        for line in f:
+            for ch in line:
+                cointTossList.append(int(ch))
+
+
+def writeforGraphs(TotalMessages):
+    with open('DuplexResults.txt', 'a') as f:
+        f.write(str(TotalMessages) + "\n")
+
+
+# variables needed in the simulation
+user1Coins = int(cfg.get('Lightning_Variables','user1coins'))
+user2Coins = int(cfg.get('Lightning_Variables','user2coins'))
+amount_to_transfer = int(cfg.get('Lightning_Variables','amount_to_transfer'))
+_rounds = 0
+lightningResetOccurred = 0
+cointTossList = []
+i = 0
+coinToss = 0
+_sender = object
+_receiver = object
+refill_Occurred = []
+
+# create two objects of the user class
+alice = User(user1Coins, 'alice')
+bob = User(user2Coins, 'bob')
+
+
+# input required for the number of payments
+print("LIGHTNING DUPLEX SIMULATION")
+readFromFile = int(input("Do you want to read coin toss from the file (1 YES , 0 NO): "))
+if readFromFile == 1:
+    coin_Toss()
+    no_payments = len(cointTossList)
+else:
+    no_payments = int(input("Enter the number of payments you want to Simulate: "))
+payWithReset = int(input("Choose one of the two payment methods. \n 1. Payment + Reset Together \n 2. Payment and Reset Seperate: "))
+
+
+# for loop to run X times
+for x in range(no_payments):
+    if readFromFile == 1:
+        coinToss = cointTossList[i]
+        if coinToss == 0:
+            _sender = alice
+            _receiver = bob
+        else:
+            _sender = bob
+            _receiver = alice
+        i += 1
+
+    else:
+        coinToss = random.randint(0,1)
+        if coinToss == 0:
+            _sender = alice
+            _receiver = bob
+        else:
+            _sender = bob
+            _receiver = alice
+
+    if onewayChannel(_sender, _receiver, amount_to_transfer):
+        print("One way Transfer Occurred")
+        print()
+    else:
+        if validate_Uncommitted_Coins(_sender):
+            if payWithReset == 1:
+                lightningResetWithPayment(_sender, _receiver, amount_to_transfer)
+            else:
+                lightningResetWithoutPayment(_sender,_receiver,amount_to_transfer)
+        else:
+            lightningResetWithoutPayment(_sender,_receiver,amount_to_transfer)
+            if refillChannel(_receiver,_sender):
+                print("Refill Occurred ")
+                refill_Occurred.append(x)
+            lightningResetWithoutPayment(_sender, _receiver, amount_to_transfer)
+
+    writeforGraphs(alice.messages+bob.messages)
+    print(alice.name, "has ", alice.coins, " coins to send and has received ", alice.deposited_coins,
+          " uncommitted coins and ", bob.name, " has ",
+          bob.coins, " coins to send and has received ", bob.deposited_coins, " uncommitted Coins")
+printResults()
